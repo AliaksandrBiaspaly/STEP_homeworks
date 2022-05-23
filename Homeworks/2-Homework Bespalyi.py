f352bbc5f9f2ad0886d9d72290fac7eb3bde3cda
Aleks <105670476+AliaksandrBiaspaly@users.noreply.github.com>
2022-05-23 20:58:08 +0300

    edit

--- Homeworks/2-Homework Bespalyi.py
+++ Homeworks/2-Homework Bespalyi.py
@@ -1,5 +1,4 @@
 # Написать список имен, вывести только женские имена
-import math
 
 list_name = ['Svetlana', 'Anton', 'Matthew', 'Jessica', 'Amanda']
 print(list_name[0], list_name[3], list_name[4])
