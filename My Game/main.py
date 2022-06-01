fd631a70d348d7f926dad6dabe123eef19240a41
Aleks <105670476+AliaksandrBiaspaly@users.noreply.github.com>
2022-06-01 20:19:37 +0300

    'initial'

---
+++ My Game/main.py
@@ -0,0 +1,13 @@
+
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((1200, 800))
+
+while true:
+    for event in pygame.event.get():
+        if nevent.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
