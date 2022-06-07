92960fac259913f5317a003f9a47766abbdf2bfd
Aleks <105670476+AliaksandrBiaspaly@users.noreply.github.com>
2022-06-07 10:38:56 +0300

    Added calc

---
+++ Homeworks/4.2 - Homework Bespalyi Calculator.py
@@ -0,0 +1,16 @@
+print("Welcome to the best calc in the world you've ever seen")
+print("You can always thank to the creator Aleks Bespalyi")
+print()
+
+q1 = float(input("Enter first number :"))
+operation = str(input("Enter operation of calculation:\n * - multiplay \n \ - divine \n + - plus \n - - minus \n"))
+q2 = float(input("Enter second number :"))
+
+if operation == '*':
+    print('Answer is ', q1, operation, q2, ' = ', q1 * q2)
+if operation == '/':
+    print('Answer is ', q1, operation, q2, ' = ', q/q2)
+if operation == '+':
+    print('Answer is ', q1, operation, q2, ' = ', q1 + q2)
+if operation == '-':
+    print('Answer is ', q1, operation, q2, ' = ', q1 - q2)
