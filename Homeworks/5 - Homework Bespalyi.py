5ec603fffc55fcf6f66b9f8b95e0263b737a81b0
Aleks <105670476+AliaksandrBiaspaly@users.noreply.github.com>
2022-06-13 18:03:10 +0300

    task 6 added

--- Homeworks/5 - Homework Bespalyi.py
+++ Homeworks/5 - Homework Bespalyi.py
@@ -1,67 +1,20 @@
-# Создадим функцию с простыми командами Обернём её в декоратор, который бы дополнял возможности функции.
+# 1 Написать типы для всех задач из прошлого дз
 
-def bonus_Petrov(Petrov):
-    def wrapper():
-        Petrov()
-        print("Year bonus salary is 8000$ ")
-        print("Personal bonus achievement is 3000$ ")
+from typing import TYPE_CHECKING
 
-    return wrapper()
+def exchange_currency(byn: float) -> float:
+    return byn * 2.58
 
 
-@bonus_Petrov
-def salary_Petrov():
-    x = str(input("Enter the surname :"))
-    return print("Month salary is 1500$") if x == "Petrov" else print('nothing found')
+def sum(a: int, b: int) -> int:
+    return a + b
 
 
-print(salary_Petrov())
-
-# Использовать функцию map и filter
-
-def exchange_currency(byn) :
-    return byn*2.58
-
-x = [2, 34, 10, 20000, 539]
-y = list(map(exchange_currency, x))
-print(y)
+def sum_1(x: int) -> int :
+    return x * random.randrange(1, 25)
 
 
-def odd_numbers(x):
-    if x % 2 == 0 :
-        return False
-    else :
-        return True
-
-numbers = [1 ,2 , 3, 4, 5 ,6, 7, 8, 9, 10]
-odd_result = list(filter(odd_numbers, numbers ))
-
-print (odd_result)
-print(type(odd_result))
-
-# Создадим чистую и нечистую функцию.
-import random
-
-#pure function
-def sum(a,b):
-    return a+b
-print(sum(4,6))
-print(sum(4,6))
-print(sum(4,6))
-
-#not pure function
-def sum_1(x):
-    return x*random.randrange(1,25)
-print(sum_1(1))
-print(sum_1(1))
-print(sum_1(1))
-
-
-#Написать функцию, которая определяет, является ли год високосным. Пользователь
-#вводит год, если он високосный, то функция возвращает True. Если нет, то False
-
-
-def leap_year(year:int):
+def leap_year(year: int) -> int:
     if year % 4 == 0 and year % 100 != 0:
         return True
     elif year % 400 == 0:
@@ -69,29 +22,24 @@ def leap_year(year:int):
     else:
         return False
 
-x = int(input("Enter a year: "))
-print(leap_year(x))
 
-
-# Сделать функцию поиска минимума и максимума в списке.
-
-def maximum(data_list):
+def maximum(data_list: list) -> int:
     return max(data_list)
 
-data_list = [2, 5, 6, 33, 78,4, 45, -3]
+
+data_list = [2, 5, 6, 33, 78, 4, 45, -3]
 print(maximum(data_list))
 
 
-def minimum(data_l):
+def minimum(data_l: list):
     return min(data_l)
 
-data_l = [2, 5, 6, 33, 78,4, 45, -3]
+
+data_l = [2, 5, 6, 33, 78, 4, 45, -3]
 print(minimum(data_l))
 
-# Написать функцию season, принимающую 1 аргумент — номер месяца (от 1 до 12), и
-# возвращающую время года, которому этот месяц принадлежит (зима, весна, лето или осень)
 
-def season(month_num):
+def season(month_num: int) -> None:
     if 6 > month_num > 2:
         return print("Spring")
     elif 9 > month_num > 5:
@@ -101,25 +49,72 @@ def season(month_num):
     else:
         return print("Winter")
 
-month = int(input("Enter month number or 999 for exit: "))
 
-while month > 12:
-    if month > 12 and month != 999 :
-        month = int(input("Enter month number from 1 to 12 \nor 999 for exit: "))
-    # print(season(month))
-    else:
-        quit(month)
+def just_int_float(a: list) -> list:
+    b = list(filter(lambda x: isinstance(x, int), a))
+    c = list(filter(lambda x: isinstance(x, float), a))
+    d = list(filter(lambda x: isinstance(x, bool), a))
+    result = list(set(b) - set(d)) + c
+    z = sorted(result)
+    print(z)
 
-quit(month) if month == 999 else print(season(month))
 
-# Написать функцию date, принимающую 3 аргумента — день, месяц и год. Вернуть True,
-# если такая дата есть в нашем календаре, и False иначе.*
+list_a = [16, -17, 2, 78.7, False, False, {"True": "True"}, 555, 12, 23, 42, 'DD']
+just_int_float(list_a)
 
-from datetime import date
-correctDate = None
-try:
-    newDate = date(2009,2,29)
-    correctDate = True
-except ValueError:
-    correctDate = False
-print(str(correctDate))
+
+# 2 Написать функцию типа [i: int, n: int, a: str] -> any
+
+def example(x: int, n: int, a: str) -> any:
+    a = '8'
+    n = str(n + 1)
+    return str(x) + n + a
+
+
+s = ""
+print(list(example(1, 3, s)))
+
+
+# 3 Написать функцию по типу x: str -> int
+
+def hello(x: str) -> int:
+    return int(x, 0)
+
+
+z = "0b1010"
+print((hello(z)))
+print(type(hello(z)))
+
+
+# 4 Написать функцию для суммы чисел, для нее написать декоратор, который возводит в степень результаты
+# и выводит на экран в виде (степень суммы чисел x,y = )
+
+def squard(result_sum):
+    def wrapper(x, y):
+        zz = round(result_sum(x, y) ** 2, 2)
+        print("степень суммы чисел: x= ", x, "y = ", y, " равно ", zz)
+    return wrapper
+
+
+@squard
+def sum(first: float, second: float):
+    return round(first + second, 2)
+
+
+sum(2.33, 5.4)
+sum(2.5, 5.4444)
+
+
+# 6 Для перебора массива использовать yield, достать 5 элемент массива через next()
+
+def generator_numbers(n: int):
+    for number in range(n):
+        yield number
+
+
+my_list = generator_numbers(100000)
+print(next(my_list))
+print(next(my_list))
+print(next(my_list))
+print(next(my_list))
+print(next(my_list))
