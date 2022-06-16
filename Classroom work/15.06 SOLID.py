2c024fb4429ca6162f45c1f2f38b04c3f92dd597
Aleks <105670476+AliaksandrBiaspaly@users.noreply.github.com>
2022-06-16 19:36:01 +0300

    commit

---
+++ Classroom work/15.06 SOLID.py
@@ -0,0 +1,21 @@
+from email.header import UTF8
+
+file = open('tekst.txt', 'r', encoding="utf8")
+print(file.read(120))
+file.close()
+
+# создает новый файл, если его не существует
+file = open('tekst2.txt', 'w', encoding="utf-8")
+print(file.write('RUSS фвыф'))
+file.close()
+
+# то же самое, только короче
+with open('tekst.txt', 'w', encoding="utf8") as file:
+    print(file.read())
+
+with open('tekst.txt', 'w', encoding="utf8") as file:
+    for i in file:
+        print(i)
+
+
+# SOLID
