7f19334959d0927048c772d8d97edbe3b0ac3293
Aleks <105670476+AliaksandrBiaspaly@users.noreply.github.com>
2022-07-04 17:16:23 +0300

    completed

--- Homeworks/8 - Homework Bespalyi SQL/8 - Homework Bespalyi SQL.py
+++ Homeworks/8 - Homework Bespalyi SQL/8 - Homework Bespalyi SQL.py
@@ -7,12 +7,15 @@ import sqlite3 as sql
 В следующий раз, когда мы запустим этот файл ****.py, он просто подключается к базе данных,
 и если базы данных нет, он создаст ее. '''
 connection = sql.connect('employees.db')
+if connection:
+    print("Подключен к SQLite")
 
 '''3) Создайте таблицу базы данных
 Чтобы создать таблицу в базе данных, нам нужно использовать объект курсора.
 Чтобы создать объект курсора, используйте метод connection.cursor().
 С помощью этого объекта курсора мы теперь можем выполнять команды и запросы в базе данных.'''
 cursor = connection.cursor()
+print("Переменные Python успешно вставлены в таблицу Employees")
 
 ''' 4) Наша первая команда - создать таблицу Employees.
     Используйте  метод cursor.execute(),  чтобы написать запрос CREATE TABLE.  
@@ -51,9 +54,6 @@ connection.commit()
                         """ )'''
 connection.commit()
 
-# Получить список с информацией обо всех сотрудниках
-cursor.execute("SELECT * FROM Employees")
-
 
 # Добавляем столбец с именем, который забыли, и вносим в эту колонку данные
 #cursor.execute("ALTER TABLE Employees ADD COLUMN firstname TEXT")
@@ -68,11 +68,31 @@ cursor.execute("SELECT * FROM Employees")
 # cursor.execute("UPDATE Employees SET firstname = 'David' WHERE employee_id = 9")
 # cursor.execute("UPDATE Employees SET firstname = 'Anton' WHERE employee_id = 10")
 
+# Добавляем столбец с именем, который забыли, и вносим в эту колонку данные
+#cursor.execute("ALTER TABLE Employees ADD COLUMN start_date DATE")
+# cursor.execute("UPDATE Employees SET start_date = '2012-09-22' WHERE employee_id = 1")
+# cursor.execute("UPDATE Employees SET start_date = '2002-02-12' WHERE employee_id = 2")
+# cursor.execute("UPDATE Employees SET start_date = '2022-08-22' WHERE employee_id = 3")
+# cursor.execute("UPDATE Employees SET start_date = '2020-01-01' WHERE employee_id = 4")
+# cursor.execute("UPDATE Employees SET start_date = '2017-08-06' WHERE employee_id = 5")
+# cursor.execute("UPDATE Employees SET start_date = '2012-08-30' WHERE employee_id = 6")
+# cursor.execute("UPDATE Employees SET start_date = '2019-08-13' WHERE employee_id = 7")
+# cursor.execute("UPDATE Employees SET start_date = '2010-03-03' WHERE employee_id = 8")
+# cursor.execute("UPDATE Employees SET start_date = '2021-11-04' WHERE employee_id = 9")
+# cursor.execute("UPDATE Employees SET start_date = '2021-12-07' WHERE employee_id = 10")
+
+# вносим изменения в номер департамента
+# cursor.execute("UPDATE Employees SET department_id = '70' WHERE employee_id = 8")
+# cursor.execute("UPDATE Employees SET department_id = '70' WHERE employee_id = 9")
+# cursor.execute("UPDATE Employees SET department_id = '70' WHERE employee_id = 10")
 # connection.commit()
 
 #** Удалить лишние строки пустые
 # cursor.execute("DELETE FROM Employees WHERE name IS NULL")
 # connection.commit()
+
+# 1) Получить список с информацией обо всех сотрудниках
+cursor.execute("SELECT * FROM Employees")
 
 # 2) Получить список всех сотрудников с именем David
 cursor.execute("SELECT * FROM Employees WHERE firstname = 'David'")
@@ -89,14 +109,20 @@ cursor.execute("SELECT * FROM Employees WHERE name LIKE '%a'")
 # 6) В фамилии содержится 2 буквы n
 cursor.execute("SELECT * FROM Employees WHERE name LIKE '%n%n%'")
 
-# 7) Получить отчет по департаменту с мин и макс зп,
+# 7) Получить отчет по департаменту с мин и макс зп, c ранней и поздней датой прихода на работу и количества сотрудниковб
+# Сортировать по количеству сотрудников по убыванию
+cursor.execute("""SELECT department_id,min(salary),max(salary),min(start_date),max(start_date), COUNT(department_id) FROM Employees
+     GROUP BY department_id
+     HAVING COUNT(department_id)
+	 ORDER BY count(department_id) DESC;""")
 
 
+# 8) Получить список manager_id  у которых количество подчиненных больше 2 и сумма всех за подчиненных больше 9000
+cursor.execute("""SELECT manager_id,sum(salary), COUNT(manager_id) FROM Employees
+     GROUP BY manager_id
+     HAVING COUNT(manager_id) > 2 AND sum(salary)> 9000;""")
 
 
-
-
-
-
-''' 6) Закрыть соединение с БД'''
-#connection.close()
+'''Закрыть соединение с БД'''
+connection.close()
+print("Соединение разорвано")
