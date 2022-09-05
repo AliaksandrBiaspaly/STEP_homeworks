983c15a652ffdc609367f02c3ca7bfc90693cf36
Aleks <105670476+AliaksandrBiaspaly@users.noreply.github.com>
2022-09-05 22:55:12 +0300

    Commit

--- My Game/Xmas race/constant.py
+++ My Game/Xmas race/constant.py
@@ -16,7 +16,7 @@ display.set_caption("Xmas race")  # название
 # цвета
 # Colors used in project
 black = (0, 204, 204)
-white = (238, 204, 204)
+white = (108, 130, 133)
 red = (255, 0, 0)
 green = (0, 200, 0)
 bright_green = (255, 0, 0)
